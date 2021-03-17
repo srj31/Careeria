5bae051d3b6aa8b7b916cfa6847c5695f7a322ca
srj31 <sourabhrj31@gmail.com>
2021-03-17 22:07:26 +0800

    Landing update

--- client/src/components/layout/Landing.js
+++ client/src/components/layout/Landing.js
@@ -13,10 +13,9 @@ const Landing = ({isAuthenticated}) => {
     <section className="landing">
       <div className="dark-overlay">
         <div className="landing-inner">
-          <h1 className="x-large">Connecting Developers</h1>
+          <h1 className="x-large">Connecting Dots</h1>
           <p className="lead">
-            Create a developer profile/portfolio, share posts, have discussions and get help from
-            other developers
+            Let people meet their destination...
           </p>
           <div className="buttons">
             <Link to="/register" className="btn btn-primary">
